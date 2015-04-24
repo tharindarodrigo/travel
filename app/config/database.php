fe660a5fa0941c7309cca3eb3cadbda537ff607d
Tharinda <tharindarodrigo@gmail.com>
2015-04-24 11:21:23 +0530

    added new migrations

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '192.168.2.226',
+			'host'      => '',
 			'database'  => 'exotic',
 			'username'  => 'hasitha',
 			'password'  => '123',
