e4b926917f6ee51e33997f81533e63c66649898e
THELIyA <thilinalink@gmail.com>
2015-04-28 18:27:34 +0530

    Edited navbar 1.1

--- app/views/layout/navbar.blade.php
+++ app/views/layout/navbar.blade.php
@@ -2,25 +2,181 @@
     .logo {
         height: 50px;;
     }
+
+    .popover {
+        z-index: 10000 !important;
+    }
 </style>
 
+<!-- jQuery OnlineLink -->
+{{ HTML::script('//code.jquery.com/jquery-1.11.2.min.js') }}
+{{ HTML::script('//code.jquery.com/jquery-migrate-1.2.1.min.js') }}
+
+<!-- Bootstrap -->
+{{ HTML::style('dist/css/bootstrap.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+{{ HTML::style('assets/css/custom.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+
+{{ HTML::script('dist/js/bootstrap.min.js') }}
+
+<style type="text/css">
+    body {
+        padding-top: 90px;
+    }
+
+    .panel-login {
+        border-color: #ccc;
+        -webkit-box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.2);
+        -moz-box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.2);
+        box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.2);
+    }
+
+    .panel-login > .panel-heading {
+        color: #00415d;
+        background-color: #fff;
+        border-color: #fff;
+        text-align: center;
+    }
+
+    .panel-login > .panel-heading a {
+        text-decoration: none;
+        color: #666;
+        font-weight: bold;
+        font-size: 15px;
+        -webkit-transition: all 0.1s linear;
+        -moz-transition: all 0.1s linear;
+        transition: all 0.1s linear;
+    }
+
+    .panel-login > .panel-heading a.active {
+        color: #029f5b;
+        font-size: 18px;
+    }
+
+    .panel-login > .panel-heading hr {
+        margin-top: 10px;
+        margin-bottom: 0px;
+        clear: both;
+        border: 0;
+        height: 1px;
+        background-image: -webkit-linear-gradient(left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0));
+        background-image: -moz-linear-gradient(left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0));
+        background-image: -ms-linear-gradient(left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0));
+        background-image: -o-linear-gradient(left, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0));
+    }
+
+    .panel-login input[type="text"], .panel-login input[type="email"], .panel-login input[type="password"] {
+        height: 45px;
+        border: 1px solid #ddd;
+        font-size: 16px;
+        -webkit-transition: all 0.1s linear;
+        -moz-transition: all 0.1s linear;
+        transition: all 0.1s linear;
+    }
+
+    .panel-login input:hover,
+    .panel-login input:focus {
+        outline: none;
+        -webkit-box-shadow: none;
+        -moz-box-shadow: none;
+        box-shadow: none;
+        border-color: #ccc;
+    }
+
+    .btn-login {
+        background-color: #59B2E0;
+        outline: none;
+        color: #fff;
+        font-size: 14px;
+        height: auto;
+        font-weight: normal;
+        padding: 14px 0;
+        text-transform: uppercase;
+        border-color: #59B2E6;
+    }
+
+    .btn-login:hover,
+    .btn-login:focus {
+        color: #fff;
+        background-color: #53A3CD;
+        border-color: #53A3CD;
+    }
+
+    .forgot-password {
+        text-decoration: underline;
+        color: #888;
+    }
+
+    .forgot-password:hover,
+    .forgot-password:focus {
+        text-decoration: underline;
+        color: #666;
+    }
+
+    .btn-register {
+        background-color: #1CB94E;
+        outline: none;
+        color: #fff;
+        font-size: 14px;
+        height: auto;
+        font-weight: normal;
+        padding: 14px 0;
+        text-transform: uppercase;
+        border-color: #1CB94A;
+    }
+
+    .btn-register:hover,
+    .btn-register:focus {
+        color: #fff;
+        background-color: #1CA347;
+        border-color: #1CA347;
+    }
+
+</style>
+
+<script type="text/javascript">
+    $(function () {
+
+        $('#login-form-link').click(function (e) {
+            $("#login-form").delay(100).fadeIn(100);
+            $("#register-form").fadeOut(100);
+            $('#register-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+        $('#register-form-link').click(function (e) {
+            $("#register-form").delay(100).fadeIn(100);
+            $("#login-form").fadeOut(100);
+            $('#login-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+
+    });
+
+</script>
+
 <div class="row">
-    <div class="col-md-6"></div>
-    <div class="col-md-6">
+    <div class="col-md-7"></div>
+    <div class="col-md-5">
         <table align="center">
             <tr>
                 <th style="padding: 10px;"><a href="" style="text-decoration: none">Language</a></th>
                 <th style="padding: 10px;"><a href="" style="text-decoration: none">USD</a></th>
                 <th style="padding: 10px;">
-                    <a href="" style="text-decoration: none">Sign in</a>
+
+                    <a style="text-decoration: none" href="{{URL::to('/account/sign-in')}}">
+                        Sign in
+                    </a>
                     or
-                    <a href="" style="text-decoration: none">Sign up</a>
+                    <a style="text-decoration: none" href="{{URL::to('/account/create')}}">
+                        Sign up
+                    </a>
+
                 </th>
             </tr>
         </table>
     </div>
 </div>
-
 
 <div class="container">
     <div class="navbar mtnav">
@@ -55,7 +211,7 @@
                             ?>
                             @foreach($hotel_type as $hotel_categories)
                                 <li>
-                                    <a href="{{URL::to('/accommodation/'.str_replace(' ', '-',$hotel_categories->hotel_category)) }}">{{ $hotel_categories->hotel_category }}</a>
+                                    <a href="{{URL::to('/accommodation/'.str_replace(' ', '-',$hotel_categories->name)) }}">{{ $hotel_categories->name }}</a>
                                 </li>
                             @endforeach
                         </ul>
