40b496009a5de375ef6b2c2dd821e22b5a5c011d
Tharinda <tharindarodrigo@gmail.com>
2015-04-28 17:36:13 +0530

    changed register and sign-in views

--- app/views/account/register.blade.php
+++ app/views/account/register.blade.php
@@ -1,175 +1,196 @@
-@extends('layout.master')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title')
+<head>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> srilankahotel.travel - About Us</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Registration Form </title>
 
-@endsection
+    <!-- CSS -->
+    {{ HTML::style('http://fonts.googleapis.com/css?family=Roboto:400,100,300,500.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/bootstrap/css/bootstrap.min.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/font-awesome/css/font-awesome.min.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/css/form-elements.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/css/style.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
 
-@section('content')
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    {{ HTML::script('https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js') }}
+    {{ HTML::script('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') }}
 
-    <body>
+    <style type="text/css">
+        .login_logo {
+            width: 205px !important;
+        }
 
-    <!-- 100% Width & Height container  -->
-    <div class="login-fullwidith">
+        .error {
+            font-weight: bold;
+            color: #555;
+            border-radius: 10px;
+            font-family: Tahoma, Geneva, Arial, sans-serif;
+            font-size: 11px;
+            padding: 10px 36px;
+            margin-top: 20px;
+            background: #ffecec url('../images/warning.png') no-repeat;
+            background-size: 30px 30px;
+            border: 1px solid #f5aca6;
+        }
 
-        <!-- Login Wrap  -->
-        <div class="login-wrap">logpadding
-            {{ HTML::image('images/site/logo.png', '',  array('class' => 'login-img')) }} <br/>
+    </style>
+    <![endif]-->
+</head>
 
-            <div class="login-c1">
-                <div class="cpadding50">
+<body>
 
-                    <form action="{{ URL::route('account-create-post') }}" method="POST">
+<!-- Top menu -->
+<nav class="navbar navbar-inverse navbar-no-bg" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                {{ HTML::image('images/site/logo.png', '',  array('class' => 'login_logo')) }}
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="top-navbar-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+							<span class="li-social">
+								<a href="#"><i class="fa fa-facebook"></i></a> 
+								<a href="#"><i class="fa fa-twitter"></i></a> 
+								<a href="#"><i class="fa fa-envelope"></i></a> 
+								<a href="#"><i class="fa fa-skype"></i></a>
+							</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-                        <div class="input-group">
+<!-- Top content -->
+<div class="top-content">
 
-                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                            <input type="text" class="form-control" placeholder="First Name"
-                                   name="first_name"{{ (Input::old('first_name')) ? ' value="' . e(Input::old('first_name'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if ($errors->has('first_name'))
-                                {{ '<br>'.$errors->First('first_name', '<small class=error>:message</small>') }}
-                            @endif
+    <div class="inner-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 text">
+                    <h1> Registration Form </h1>
 
-                        </div>
-                        <br/>
-
-                        <div class="input-group">
-
-                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                            <input type="text" class="form-control" placeholder="Last Name"
-                                   name="last_name"{{ (Input::old('last_name')) ? ' value="' . e(Input::old('last_name'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if($errors->has('last_name'))
-                                {{ '<br>'.$errors->First('last_name', '<small class=error>:message</small>') }}
-                            @endif
-                        </div>
-                        <br/>
-
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                            <input type="email" class="form-control" placeholder="Email"
-                                   name="email"{{ (Input::old('email')) ? ' value="' . e(Input::old('email'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if($errors->has('email'))
-                                {{ '<br>'.$errors->First('email', '<small class=error>:message</small>') }}
-                            @endif
-                        </div>
-
-                        <br>
-
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-mobile"></i></span>
-                            <input type="text" class="form-control" placeholder="Phone Number"
-                                   name="phone"{{ (Input::old('phone')) ? ' value="' . e(Input::old('phone'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if($errors->has('phone'))
-                                {{ '<br>'.$errors->First('phone', '<small class=error>:message</small>') }}
-                            @endif
-                        </div>
-                        <br>
-
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                            <input type="text" class="form-control" placeholder="Address"
-                                   name="address"{{ (Input::old('address')) ? ' value="' . e(Input::old('address'))  . '"' : ''  }}>
-                        </div>
-                        <br>
-
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                            <input type="password" class="form-control" placeholder="Password"
-                                   name="password"{{ (Input::old('password')) ? ' value="' . e(Input::old('password'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if($errors->has('password'))
-                                {{ '<br>'.$errors->First('password', '<small class=error>:message</small>') }}
-                            @endif
-                        </div>
-                        <br>
-
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-refresh"></i></span>
-                            <input type="password" class="form-control" placeholder="Repeat Password"
-                                   name="password_conf"{{ (Input::old('password_conf')) ? ' value="' . e(Input::old('password_conf'))  . '"' : ''  }}>
-                        </div>
-                        <div>
-                            @if($errors->has('password_again'))
-                                {{ '<br>'.$errors->First('password_again', '<small class=error>:message</small>') }}
-                            @endif
-                        </div>
-                        <br>
-
-                        {{--<div class="g-recaptcha" data-sitekey=""></div>--}}
-                        {{----}}
-                        {{--<?php--}}
-                        {{--require_once('../public/recaptcha/recaptchalib.php');--}}
-                        {{--$public_key = "6LeVCwQTAAAAANjhK2U0LgGbAhJHLV6nZ7oOPTuM"; // you got this from the signup page--}}
-                        {{--echo recaptcha_get_html($public_key);--}}
-                        {{--?>--}}
-
-                        @if((Session::get('global')))
-                            <p class="error">{{ Session::get('global') }}</p>
-                        @endif
-                        <br/>
-
-                        <button type="submit" class="btn btn-primary">Register Now</button>
-
-                        {{ Form::token() }}
-
-                    </form>
-
-                </div>
-            </div>
-
-            <div class="login-c2">
-                <div class="logmargfix">
-                    <div class="chpadding50">
-                        <div class="alignbottom">
-                            <button class="btn-search4" type="submit" onclick="errorMessage()">Submit</button>
-                        </div>
-                        <div class="alignbottom2">
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox">Remember
-                                </label>
-                            </div>
-                        </div>
+                    <div class="description">
+                        <p>
+                            This is the place you can register into our site.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="login-c3">
-                <div class="left"><a href="#" class="whitelink"><span></span>Website</a></div>
-                <div class="right"><a href="#" class="whitelink">Lost password?</a></div>
+            <div class="row">
+                <div class="col-sm-6 book">
+                    {{ HTML::image('login/img/ebook.png', '',  array('class' => 'logo')) }}
+                </div>
+                <div class="col-sm-5 form-box">
+                    <div class="form-top">
+                        <div class="form-top-left">
+                            <h3> Register </h3>
+                        </div>
+                        <div class="form-top-right">
+                            <i class="fa fa-pencil"></i>
+                        </div>
+                    </div>
+                    <div class="form-bottom">
+                        <form role="form" action="{{ URL::route('account-create-post') }}" method="post"
+                              class="">
+
+                            <div class="form-group">
+                                <label class="sr-only" for="form-first-name">First name</label>
+                                <input type="text" placeholder="First name..." class="form-first-name form-control"
+                                       id="form-first-name"
+                                       name="first_name"{{ (Input::old('first_name')) ? ' value="' . e(Input::old('first_name'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if ($errors->has('first_name'))
+                                    {{ $errors->First('first_name', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label class="sr-only" for="form-last-name">Last name</label>
+                                <input type="text" placeholder="Last name..." class="form-last-name form-control"
+                                       id="form-last-name"
+                                       name="last_name"{{ (Input::old('last_name')) ? ' value="' . e(Input::old('last_name'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('last_name'))
+                                    {{ $errors->First('last_name', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label class="sr-only" for="form-email">Email</label>
+                                <input type="text" placeholder="Email..."
+                                       class="form-email form-control" id="form-email"
+                                       name="email"{{ (Input::old('email')) ? ' value="' . e(Input::old('email'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('email'))
+                                    {{ $errors->First('email', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label class="sr-only" for="form-password"> Password </label>
+                                <input type="password" placeholder="Password..."
+                                       class="form-password form-control" id="form-password"
+                                       name="password"{{ (Input::old('password')) ? ' value="' . e(Input::old('password'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('password'))
+                                    {{ $errors->First('password', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label class="sr-only" for="form-password"> Confirm Password </label>
+                                <input type="password" placeholder="Confirm Password..."
+                                       class="form-password form-control" id="form-password"
+                                       name="password_conf"{{ (Input::old('password_conf')) ? ' value="' . e(Input::old('password_conf'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('password_conf'))
+                                    {{ $errors->First('password_conf', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
+
+                            <button type="submit" class="btn"> Sign In</button>
+                            {{ Form::token() }}
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <!-- End of Login Wrap  -->
-
     </div>
-    <!-- End of Container  -->
+
+</div>
 
 
-    @section('script')
-        <!-- Javascript  -->
-        {{ HTML::script('assets/js/initialize-loginpage.js') }}
-        {{ HTML::script('assets/js/jquery.easing.js') }}
-        <!-- Load Animo -->
-        {{ HTML::script('plugins/animo/animo.js') }}
+<!-- Javascript -->
+{{ HTML::script('login/js/jquery-1.11.1.min.js') }}
+{{ HTML::script('login/bootstrap/js/bootstrap.min.js') }}
+{{ HTML::script('login/js/jquery.backstretch.min.js') }}
+{{ HTML::script('login/js/retina-1.1.0.min.js') }}
+{{ HTML::script('login/js/scripts.js') }}
 
-        <script>
-            function errorMessage() {
-                $('.login-wrap').animo({animation: 'tada'});
-            }
-        </script>
+<!--[if lt IE 10]>
+{{ HTML::script('login/js/placeholder.js') }}
+<![endif]-->
 
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        {{ HTML::script('dist/js/bootstrap.min.js') }}
-    @endsection
-    </body>
+</body>
+
+</html>
--- app/views/account/sign-in.blade.php
+++ app/views/account/sign-in.blade.php
@@ -1,95 +1,159 @@
-@extends('layout.master')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title')
+<head>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> srilankahotel.travel - Sign In</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Sign-in Form </title>
 
-@endsection
+    <!-- CSS -->
+    {{ HTML::style('http://fonts.googleapis.com/css?family=Roboto:400,100,300,500.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/bootstrap/css/bootstrap.min.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/font-awesome/css/font-awesome.min.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/css/form-elements.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    {{ HTML::style('login/css/style.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
 
-@section('custom_style')
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    {{ HTML::script('https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js') }}
+    {{ HTML::script('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') }}
 
-    {{ HTML::style('plugins/animo/animate+animo.css' , array('rel' => 'stylesheet' , 'media' => 'screen')) }}
+    <style type="text/css">
+        .login_logo {
+            width: 205px !important;
+        }
 
-@endsection
+        .error {
+            font-weight: bold;
+            color: #555;
+            border-radius: 10px;
+            font-family: Tahoma, Geneva, Arial, sans-serif;
+            font-size: 11px;
+            padding: 10px 36px;
+            margin-top: 20px;
+            background: #ffecec url('../images/warning.png') no-repeat;
+            background-size: 30px 30px;
+            border: 1px solid #f5aca6;
+        }
+
+    </style>
+    <![endif]-->
+</head>
 
 <body>
 
-@section('content')
-    <!-- 100% Width & Height container  -->
-    <div class="login-fullwidith">
+<!-- Top menu -->
+<nav class="navbar navbar-inverse navbar-no-bg" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">
+                {{ HTML::image('images/site/logo.png', '',  array('class' => 'login_logo')) }}
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="top-navbar-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+							<span class="li-social">
+								<a href="#"><i class="fa fa-facebook"></i></a> 
+								<a href="#"><i class="fa fa-twitter"></i></a> 
+								<a href="#"><i class="fa fa-envelope"></i></a> 
+								<a href="#"><i class="fa fa-skype"></i></a>
+							</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-        <!-- Login Wrap  -->
-        <div class="login-wrap">
-            {{ HTML::image('images/site/logo.png', '',  array('class' => 'login-img')) }}
+<!-- Top content -->
+<div class="top-content">
 
-            <form action="{{ URL::route('account-sign-in-post') }}" method="POST">
+    <div class="inner-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 text">
+                    <h1> Sign In Form </h1>
 
-                <div class="login-c1">
-                    <div class="cpadding50">
-                        <input type="email" class="form-control logpadding" placeholder="Email"
-                               name="log_email"{{ (Input::old('log_email')) ? ' value="' . e(Input::old('log_email'))  . '"' : ''  }}>
-                        @if($errors->has('log_email'))
-                            {{ '<br>'.$errors->First('log_email', '<small class=error>:message</small>') }}
-                        @endif
-                        <br/>
-                        <input type="password" class="form-control logpadding" placeholder="Password"
-                               name="log_password"{{ (Input::old('log_password')) ? ' value="' . e(Input::old('log_password'))  . '"' : ''  }}>
-                        @if($errors->has('log_password'))
-                            {{ '<br>'.$errors->First('log_password', '<small class=error>:message</small>') }}
-                        @endif
+                    <div class="description">
+                        <p>
+                            This is the place you can register into our site.
+                        </p>
                     </div>
                 </div>
-
-                <div class="login-c2">
-                    <div class="logmargfix">
-                        <div class="chpadding50">
-                            <div class="alignbottom">
-                                <button class="btn-search4" type="submit" onclick="errorMessage()">Submit</button>
-                            </div>
-                            <div class="alignbottom2">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox">Remember
-                                    </label>
-                                </div>
-                            </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 book">
+                    {{ HTML::image('login/img/ebook.png', '',  array('class' => 'logo')) }}
+                </div>
+                <div class="col-sm-5 form-box">
+                    <div class="form-top">
+                        <div class="form-top-left">
+                            <h3> Sign In </h3>
+                        </div>
+                        <div class="form-top-right">
+                            <i class="fa fa-pencil"></i>
                         </div>
                     </div>
-                </div>
+                    <div class="form-bottom">
+                        <form role="form" action="{{ URL::route('account-sign-in-post') }}" method="post"
+                              class="">
+                            <div class="form-group">
+                                <label class="sr-only" for="form-email">Email</label>
+                                <input type="text" placeholder="Email..."
+                                       class="form-email form-control" id="form-email"
+                                       name="log_email"{{ (Input::old('log_email')) ? ' value="' . e(Input::old('log_email'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('log_email'))
+                                    {{ $errors->First('log_email', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
 
-                <div class="login-c3">
-                    <div class="left"><a href="#" class="whitelink"><span></span>Website</a></div>
-                    <div class="right"><a href="#" class="whitelink">Lost password?</a></div>
-                </div>
-                {{ Form::token() }}
-            </form>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-password"> Password </label>
+                                <input type="text" placeholder="Password..."
+                                       class="form-password form-control" id="form-password"
+                                       name="log_password"{{ (Input::old('log_password')) ? ' value="' . e(Input::old('log_password'))  . '"' : ''  }}>
+                            </div>
+                            <div>
+                                @if($errors->has('log_password'))
+                                    {{ $errors->First('log_password', '<small class=error>:message</small>') }}
+                                @endif
+                            </div>
 
+                            <button type="submit" class="btn"> Sign In</button>
+                            {{ Form::token() }}
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- End of Login Wrap  -->
-
     </div>
-    <!-- End of Container  -->
 
-@endsection
+</div>
 
-@section('script')
 
-    <!-- Javascript  -->
-    {{ HTML::script('assets/js/initialize-loginpage.js') }}
-    {{ HTML::script('assets/js/jquery.easing.js') }}
-    <!-- Load Animo -->
-    {{ HTML::script('plugins/animo/animo.js') }}
+<!-- Javascript -->
+{{ HTML::script('login/js/jquery-1.11.1.min.js') }}
+{{ HTML::script('login/bootstrap/js/bootstrap.min.js') }}
+{{ HTML::script('login/js/jquery.backstretch.min.js') }}
+{{ HTML::script('login/js/retina-1.1.0.min.js') }}
+{{ HTML::script('login/js/scripts.js') }}
 
-    <script>
-        function errorMessage() {
-            $('.login-wrap').animo({animation: 'tada'});
-        }
-    </script>
-
-@endsection
+<!--[if lt IE 10]>
+{{ HTML::script('login/js/placeholder.js') }}
+<![endif]-->
 
 </body>
 
-@stop
+</html>
