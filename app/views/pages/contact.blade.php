07d6656149f37848ff83a81cd4edeb3b2ed9d235
Thilina <thilinalink@gmail.com>
2015-06-26 10:35:59 +0530

    contact us

--- app/views/pages/contact.blade.php
+++ app/views/pages/contact.blade.php
@@ -4,7 +4,7 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> srilankahotel.travel - About Us</title>
+    <title> srilankahotel.travel - Contact Us</title>
 
 @endsection
 
